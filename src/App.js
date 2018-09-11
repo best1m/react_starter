af53f2256c4f0e4852d49feccddf2544dc157621
kthiswell <kthiswell@rocode.co.kr>
2018-09-11 14:58:58 +0900

    app location change and create home.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import { Home } from './components/home';
 
 class App extends Component {
     render(){
         return(
         <div>
-            <h1>react starter!</h1>
+            <Home/>
         </div>
         )
     }
