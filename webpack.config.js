0fb7b49dbb61ba2c41a52520a2ba7e0b2f48e880
kthiswell <kthiswell@rocode.co.kr>
2018-10-15 16:15:08 +0900

    add css, sass module

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,29 @@ module.exports = {
                 use : {
                     loader : 'babel-loader'
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: "style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                        loader: "style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }
+                ]
             }
         ]
     },
